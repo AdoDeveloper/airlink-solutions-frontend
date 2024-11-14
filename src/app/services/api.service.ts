ab89f126a85ca67d588a3aece1d36e204336c8d7
AdoDeveloper <105835711+AdoDeveloper@users.noreply.github.com>
2024-11-14 01:44:09 -0600

    ajustes en el fecth de datos

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,12 +1,9 @@
 import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, of, throwError } from 'rxjs';
-import { catchError, tap, timeout } from 'rxjs/operators';
+import { Observable, throwError } from 'rxjs';
+import { catchError, timeout } from 'rxjs/operators';
 import { isPlatformServer } from '@angular/common';
-import { TransferState, makeStateKey } from '@angular/core';
 import { environment } from '../../environments/environment'; // Importar el entorno actual
-
-const SERVICIOS_KEY = makeStateKey<any[]>('servicios');
 
 @Injectable({
   providedIn: 'root',
@@ -16,32 +13,23 @@ export class ApiService {
 
   constructor(
     private http: HttpClient,
-    private transferState: TransferState,
     @Inject(PLATFORM_ID) private platformId: Object
   ) {}
 
+  // Método para obtener los servicios sin usar el caché
   getServicios(): Observable<any[]> {
-    if (this.transferState.hasKey(SERVICIOS_KEY)) {
-      // Retrieve cached services if available
-      const servicios = this.transferState.get<any[]>(SERVICIOS_KEY, []);
-      return of(servicios);
-    }
-
+    // Si estamos en el servidor (SSR), siempre recuperamos los datos frescos
     if (isPlatformServer(this.platformId)) {
-      // During SSR, fetch data and cache it in TransferState
       return this.http.get<any[]>(this.apiUrl).pipe(
         timeout(10000),
-        tap((servicios) => {
-          this.transferState.set(SERVICIOS_KEY, servicios);
-        }),
         catchError((error) => {
           console.error('Error fetching services during SSR:', error);
-          return of([]); // Return empty array on SSR error
+          return throwError(() => new Error('Error fetching services'));
         })
       );
     }
 
-    // For the client, make a regular HTTP call
+    // En el cliente, siempre hace una nueva solicitud HTTP
     return this.http.get<any[]>(this.apiUrl).pipe(
       catchError((error) => {
         console.error('Error fetching services:', error);
